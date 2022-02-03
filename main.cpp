a7d5b391b7652aec55bac476c74f8eb66854bfca
Tulio-bardini <tulio.bardini53@gmail.com>
2022-02-03 18:09:41 -0300

    L

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
 
     char xmlfilename[100];
+    string line;
 
     std::cin >> xmlfilename;  // entrada
-    
+
+    ifstream myfile(xmlfilename);
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile,line))
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    else cout << "Unable to open file";
+
     /*
      
        COLOQUE SEU CODIGO AQUI
     
     */
-
-    std::cout << xmlfilename << std::endl;  // esta linha deve ser removida
 
     return 0;
 }
